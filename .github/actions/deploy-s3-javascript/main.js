f4a83bf4f87a24e20f29b6523d2566a37b8af883
Mauricio Perez <Mauricio.Perez@davita.com>
2024-03-05 14:07:08 -0600

    commit

--- .github/actions/deploy-s3-javascript/main.js
+++ .github/actions/deploy-s3-javascript/main.js
@@ -14,7 +14,7 @@ function run() {
     exec.exec(`aws s3 sync ${distFolder} ${s3Uri}  --region ${bucketRegion}`);
     core.notice('Hello from my custom Javascript Action!')
 
-    const websiteurl = `http://${{bucket}}.s3-website-${{bucketRegion}}.amazonaws.com`
+    const websiteurl = `http://${bucket}.s3-website-${bucketRegion}.amazonaws.com`
     core.setOutput('website-url', websiteurl);
 }
 
